8ab72250a02df935fa3cc9aa37bf0c827ee11b13
lihong44 <lihong@chapman.edu>
2026-02-07 21:33:27 -0800

    pause, quit, and resume buttons work
    methods created to search and wire buttons upon loading a scene within UIButtonManager!!
    
    changed the quit button's name to be specific to the code.

--- Assets/Scripts/GameSceneManager.cs
+++ Assets/Scripts/GameSceneManager.cs
@@ -3,49 +3,95 @@ using UnityEditor;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-
 public class GameSceneManager : MonoBehaviour
-{ 
-   // [Header("Script References")] 
-    
-    
-   // [Header("Scene References")]
-    
-    
-    
-  
-   public enum State {Start, Battle, Pause, Results}
+{
+    public static GameSceneManager Instance { get; private set; }
 
-   public State state;
-   
-   private void Awake()
-   {
-       DontDestroyOnLoad(gameObject);
-   }
+    public enum State { Start, Battle, Pause, Results }
+    public State state;
 
-   void Start() => ChangeState(State.Start);
-       
+    private GameObject pauseShadePanel;
 
-   public void ChangeState(State newState)
-   {
-       state = newState;
-       switch (newState)
-       {
-           case State.Start:
-               if (SceneManager.GetActiveScene().name != "StartScene")
-                   SceneManager.LoadScene("StartScene");
-               break;
-           case State.Battle:
-               if (SceneManager.GetActiveScene().name != "BattleScene")
-                   SceneManager.LoadScene("BattleScene");
-               break;
-           case State.Pause:
-               
-               break;
-           case State.Results:
-               break;
-       }
-   }
-   
+    private void Awake()
+    {
+        if (Instance != null && Instance != this)
+        {
+            Destroy(gameObject);
+            return;
+        }
 
+        Instance = this;
+        DontDestroyOnLoad(gameObject);
+
+        // need this to find stuff in new scenes
+        SceneManager.sceneLoaded += OnSceneLoaded;
+    }
+
+    private void OnDestroy()
+    {
+        SceneManager.sceneLoaded -= OnSceneLoaded;
+    }
+
+    private void OnSceneLoaded(Scene scene, LoadSceneMode mode)
+    {
+        switch (scene.name)
+        {
+            case "BattleScene":
+                pauseShadePanel = GameObject.Find("PauseShadePanel");
+                // small delay so UIButtonManager wires buttons first
+                Invoke(nameof(HidePausePanel), 0.05f);
+                break;
+
+            case "StartScene":
+                pauseShadePanel = null;
+                break;
+        }
+    }
+
+    void Start() => ChangeState(State.Start);
+
+    public void ChangeState(State newState)
+    {
+        state = newState;
+        switch (newState)
+        {
+            case State.Start:
+                if (SceneManager.GetActiveScene().name != "StartScene")
+                    SceneManager.LoadScene("StartScene");
+                break;
+
+            case State.Battle:
+                if (SceneManager.GetActiveScene().name != "BattleScene")
+                    SceneManager.LoadScene("BattleScene");
+                else
+                    SetPausePanel(false);  // resuming from pause
+                break;
+
+            case State.Pause:
+                SetPausePanel(true);
+                break;
+
+            case State.Results:
+                break;
+        }
+    }
+
+    private void SetPausePanel(bool active)
+    {
+        if (pauseShadePanel == null)
+        {
+            Debug.LogWarning("PauseShadePanel not found!");
+            return;
+        }
+
+        pauseShadePanel.SetActive(active);
+    }
+    
+    private void HidePausePanel()
+    {
+        if (pauseShadePanel != null)
+            pauseShadePanel.SetActive(false);
+    }
 }
+
+
--- Assets/Scripts/UIButtonManager.cs
+++ Assets/Scripts/UIButtonManager.cs
@@ -1,12 +1,18 @@
+
+
+
+
 // UIButtonManager.cs
 using UnityEditor;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class UIButtonManager : MonoBehaviour
 {
     [Header("Script References")]
     public GameSceneManager gameSceneManager;
-    
+
     public static UIButtonManager Instance { get; private set; }
 
     private void Awake()
@@ -19,6 +25,45 @@ public class UIButtonManager : MonoBehaviour
 
         Instance = this;
         DontDestroyOnLoad(gameObject);
+
+        SceneManager.sceneLoaded += OnSceneLoaded;
+    }
+
+    private void OnDestroy()
+    {
+        SceneManager.sceneLoaded -= OnSceneLoaded;
+    }
+
+    // --- find and wire buttons when a new scene loads ---
+    private void OnSceneLoaded(Scene scene, LoadSceneMode mode)
+    {
+        switch (scene.name)
+        {
+            case "BattleScene":
+                WireBattleButtons();
+                break;
+        }
+    }
+
+    private void WireBattleButtons()
+    {
+        WireButton("PauseButton", OnPauseButtonClick);
+        WireButton("ResumeButton", OnResumeButtonClick);
+        WireButton("QuitButton", OnQuitButtonClick);
+    }
+
+    private void WireButton(string buttonName, UnityEngine.Events.UnityAction action)
+    {
+        GameObject obj = GameObject.Find(buttonName);
+        if (obj == null)
+        {
+            Debug.LogWarning($"Button '{buttonName}' not found!");
+            return;
+        }
+
+        Button button = obj.GetComponent<Button>();
+        button.onClick.AddListener(action);
+        Debug.Log($"Wired: {buttonName}");
     }
 
     // --- Main Menu ---
@@ -27,28 +72,29 @@ public class UIButtonManager : MonoBehaviour
         gameSceneManager.ChangeState(GameSceneManager.State.Battle);
     }
 
-    // --- Pause Menu ---
-    public void OnResumeButtonClick()
-    {
-        //what state to put it in to resume game
-        //gameSceneManager.ChangeState(GameSceneManager.State.Battle);
-    }
-
+    // --- Pause Menu (wired by code in BattleScene) ---
     public void OnPauseButtonClick()
     {
         Debug.Log("Game is in paused state");
         gameSceneManager.ChangeState(GameSceneManager.State.Pause);
-        
+    }
+
+    public void OnResumeButtonClick()
+    {
+        Debug.Log("Game resumed");
+        gameSceneManager.ChangeState(GameSceneManager.State.Battle);
     }
 
     // --- Shared ---
     public void OnQuitButtonClick()
     {
         #if UNITY_EDITOR
-                EditorApplication.isPlaying = false;
+        EditorApplication.isPlaying = false;
         #else
-                Application.Quit();
-                Debug.Log("Application Quit!");
+        Application.Quit();
+        Debug.Log("Application Quit!");
         #endif
     }
 }
+
+
